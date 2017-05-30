8247c0ada0d595b98a51f192ef06cc45f0c9f65e
Evandro Pires Alves <evandro@ipb.pt>
2017-05-30 16:10:48 +0100

    Initial commit

---
+++ MailManagerModule.php
@@ -0,0 +1,15 @@
+namespace evandro\modules\MailManager;
+
+class MailManagerModule extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+
+    public static function hello()
+    {
+        return 'Hello World!';
+    }
+}
+
