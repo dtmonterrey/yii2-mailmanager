7df80ce332b92910dbb91a3c45a625c1e2874456
Evandro Pires Alves <evandro@ipb.pt>
2019-04-26 15:17:21 +0100

    Version bump

--- config/console.php
+++ config/console.php
@@ -4,7 +4,7 @@
 $config = [
     'id' => 'evandro-mailmanager',
     'basePath' => dirname(__DIR__),
-    'version' => '0.2.0',
+    'version' => '0.2.1',
     'bootstrap' => ['log'],
     'controllerNamespace' => 'app\commands',
     'components' => [
